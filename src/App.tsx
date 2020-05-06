168a0548440510cd8d694e4184473fcd47988790
levan <tuanlong.levan@gmail.com>
2020-05-06 17:21:55 +0800

    Added Material Ui and Material UI icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import * as S from './App.styles';
+import AccessibleForwardIcon from '@material-ui/icons/AccessibleForward';
 
 function App() {
     return (
         <S.App>
+            <AccessibleForwardIcon
+                style={{ color: 'green', fontSize: '40px' }}
+            />
             <h1>Alpha Tech App Frontend</h1>
         </S.App>
     );
